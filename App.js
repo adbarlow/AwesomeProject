140e3e22cb413663a8368965e020c063f828b111
Adam Barlow <dadambarlow@gmail.com>
2018-05-29 22:10:52 -0700

    Edited the app.js file

--- App.js
+++ App.js
@@ -15,53 +15,8 @@ import {
 
 import Analytics from 'appcenter-analytics';
 import Crashes from 'appcenter-crashes';
+import CodePush from 'react-native-code-push';
 
-import CodePush from 'react-native-code-push'
-
-export default class AwesomeProject extends Component {
-	sendEvent(){
-		Analytics.trackEvent('My Custom Event', {
-			prop1: new Date().getSeconds()
-		});
-	}
-}
-
-nativeCrash() {
-	Crashes.generateTestCrash();
-}
-
-jsCrash() {
-	this.func1();
-}
-
-func1() { this.func2(); }
-func2() { this.func3(); }
-func3() { this.func4(); }
-func4() { this.func5(); }
-
-func5(){
-	throw new Error('My uncaught javascript exception');
-}
-
-codepushSync(){
-	this.setState({logs : ['Started at ' + new Date().getTime()]});
-	CodePush.sync({
-		updateDialog: true,
-		installMode: CodePush.installMode.IMMEDIATE
-	}, (status) => {
-		for(var key in CodePush.SyncStatus){
-			if(status === CodePush.SyncStatus[key]{
-				this.setState(preState => ({ logs: [...preState.logs, key.replace(/_/g, ' ')] }));
-				break;
-			}
-		}
-	});
-}
-
-constructor(props){
-	super(props);
-	this.state = {logs : []};
-}
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -71,18 +26,54 @@ const instructions = Platform.select({
 });
 
 // type Props = {};
-// export default class App extends Component<Props> {
+export default class AwesomeProject extends Component {
+	sendEvent() {
+		Analytics.trackEvent('My Custom Event', {
+			prop1: new Date().getSeconds()
+		});
+	}
+
+	nativeCrash() {
+		Crashes.generateTestCrash();
+	}
+
+	jsCrash() {
+		this.func1();
+	}
+
+	func1() { this.func2(); }
+	func2() { this.func3(); }
+	func3() { this.func4(); }
+	func4() { this.func5(); }
+
+	func5(){
+		throw new Error('My uncaught javascript exception');
+	}
+
+	codepushSync(){
+		this.setState({ logs: ['Started at ' + new Date().getTime()] });
+		CodePush.sync({
+			updateDialog: true,
+			installMode: CodePush.installMode.IMMEDIATE
+		}, (status) => {
+			for (var key in CodePush.SyncStatus) {
+				if (status === CodePush.SyncStatus[key]){
+					this.setState(preState => ({ logs: [...preState.logs, key.replace(/_/g, ' ')] }));
+					break;
+				}
+			}
+	});
+}
+
+constructor(props){
+	super(props);
+	this.state = { logs: [] };
+}
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
         </Text>
 		<Button title="Send Event" onPress={()=> this.sendEvent()}/>
 		<Button title="Native Crash" onPress={() => this.nativeCrash()}/>
@@ -92,7 +83,7 @@ const instructions = Platform.select({
       </View>
     );
   }
-//}
+}
 
 const styles = StyleSheet.create({
   container: {
